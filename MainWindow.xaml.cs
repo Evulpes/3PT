6d353851d4b43e3626f847bb488b1378fa8db946
Evulpes <54101182+Evulpes@users.noreply.github.com>
2020-10-12 11:33:26 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3PT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public static class Reverse
+    {
+        public enum Offsets
+        {
+            logFunc = 0xF030, //Tunnel.dll
+            sendFunc = 0x5750,
+        }
+    }
+}
